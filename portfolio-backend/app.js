511a93d95924a123db3388c4cc7c3c820ca958e0
Savan Maheshkumar Patel <sv272995@dal.ca>
2025-01-13 13:00:31 -0400

    Add : Handling Undefined Routes

--- portfolio-backend/app.js
+++ portfolio-backend/app.js
@@ -26,5 +26,9 @@ app.get('/', (req, res) => {
 app.use('/auth', authRoute);
 app.use('/projects', projectsRoute);
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
