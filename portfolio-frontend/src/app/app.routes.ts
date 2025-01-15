58867ffce465075ddc1a5280d46151d4dbb145f5
Savan Maheshkumar Patel <sv272995@dal.ca>
2025-01-15 17:11:40 -0400

    Add : Generated all the required components and setup routing for those components

--- portfolio-frontend/src/app/app.routes.ts
+++ portfolio-frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : '', component: HomeComponent},
+    { path : 'about', component: AboutComponent},
+    { path : 'experience', component: ExperienceComponent},
+    { path : 'projects', component: ProjectsComponent},
+    { path : 'contact', component: ContactComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{ };
