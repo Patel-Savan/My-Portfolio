03a4a8075806acab76fe632c58394c3bc9d37e07
Savan Maheshkumar Patel <sv272995@dal.ca>
2025-01-16 14:57:52 -0400

    Add : Responsive Navigation Bar

--- portfolio-frontend/src/app/components/navbar/navbar.component.ts
+++ portfolio-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  OnInit,
+  Renderer2,
+  signal,
+} from '@angular/core';
+import { HostListener } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, NgIf, MatIconModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  title = signal('Savan Patel');
 
+  isMobile: boolean = false;
+  menuOpened: boolean = false;
+
+  constructor(private rendered: Renderer2) {
+    this.checkScreenSize();
+  }
+  ngOnInit(): void {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+    this.toggleClass();
+  }
+  @HostBinding('class.menu-open') menuOpenClass = false;
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768;
+    if (!this.isMobile) {
+      this.menuOpened = false;
+    }
+  }
+
+  toggleMenu() {
+    this.menuOpened = !this.menuOpened;
+    this.menuOpenClass = this.menuOpened;
+
+    this.toggleClass();
+  }
+
+  toggleClass(){
+    const contentDiv = document.querySelector('#main-content');
+
+    if (this.menuOpened) {
+      this.rendered.removeClass(contentDiv, 'mobile-content');
+    } else {
+      this.rendered.addClass(contentDiv, 'mobile-content');
+    }
+  }
 }
