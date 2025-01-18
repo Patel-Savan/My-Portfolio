6390de7130e548c7bad2d3eacb31cd6fcf4ba3db
Savan Maheshkumar Patel <sv272995@dal.ca>
2025-01-18 13:05:54 -0400

    Add : Home Page

--- portfolio-frontend/src/app/components/home/home.component.ts
+++ portfolio-frontend/src/app/components/home/home.component.ts
@@ -7,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  professions: string[] = [
+    'Full Stack Developer',
+    'Cloud Developer',
+    'CS Grad'
+  ];
+  currentProfession: string = this.professions[0];
+  private professionIndex: number = 0;
+  private intervalId: any;
 
+  ngOnInit(): void {
+    this.startProfessionRotation();
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId); 
+  }
+
+  startProfessionRotation(): void {
+    this.intervalId = setInterval(() => {
+      this.professionIndex = (this.professionIndex + 1) % (this.professions.length);
+      this.currentProfession = this.professions[this.professionIndex];
+    }, 2000); 
+  }
+
+  scrollToSection(event: Event, sectionId: string) {
+    event.preventDefault();
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
