b8e699669a697f5ce859ecd49f13266bf4928871
Savan Maheshkumar Patel <sv272995@dal.ca>
2025-01-21 22:54:06 -0400

    Add : Loading Button

--- portfolio-frontend/src/main.ts
+++ portfolio-frontend/src/main.ts
@@ -3,4 +3,20 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(err))
+  .then(() => {
+    const loader = document.getElementById('app-loader');
+    if (loader) {
+      loader.classList.add('hidden');
+    }
+  });
+// .catch((err) => console.error(err))
+// .then(() => {
+//   // Simulate a delay (3 seconds) to show the splash screen
+//   setTimeout(() => {
+//     const loader = document.getElementById('app-loader');
+//     if (loader) {
+//       loader.classList.add('hidden'); // Hide the splash screen after 3 seconds
+//     }
+//   }, 3000);
+// });
