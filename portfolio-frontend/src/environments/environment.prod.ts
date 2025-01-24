54a76afd5cb12f368ab216eabd5a595f5a193652
Savan Maheshkumar Patel <sv272995@dal.ca>
2025-01-24 14:20:41 -0400

    Fix : Production build

---
+++ portfolio-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+declare var window: any;
+
+export const environment = {
+    production: false,
+    API_EMAILJS_SERVICE_ID: window.__env?.API_EMAILJS_SERVICE_ID,
+    API_EMAILJS_TEMPLATE_ID: window.__env?.API_EMAILJS_TEMPLATE_ID,
+    API_EMAILJS_USER_ID: window.__env?.API_EMAILJS_USER_ID,
+    API_BASE_URL:window.__env?.API_BASE_URL
+  };
